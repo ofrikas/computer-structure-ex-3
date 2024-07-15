b846523874e1fe1fc7cc1f0ea70f81f82db08ed9
ofrikas <ofrikas1@gmail.com>
2024-07-15 12:26:14 +0300

    Optimize vertex cover algorithm

--- optimizations/vertex-cover.c
+++ optimizations/vertex-cover.c
@@ -5,10 +5,11 @@
 #define MAX_VERTICES 100
 
 bool isVertexCover(int graph[MAX_VERTICES][MAX_VERTICES], int n, int cover[], int k) {
-    bool edges_covered[MAX_VERTICES][MAX_VERTICES] = {false};
+    bool edges_covered[MAX_VERTICES][MAX_VERTICES] = {false}; // A matrix to keep track of which edges are covered
 
+    // For each vertex in the cover, mark all edges incident to it as covered
     for (int i = 0; i < k; i++) {
-        int v = cover[i];
+        int v = cover[i]; // The vertex we're considering
         for (int j = 0; j < n; j++) {
             if (graph[v][j]) {
                 edges_covered[v][j] = edges_covered[j][v] = true;
@@ -48,9 +49,10 @@ void generateCombinations(int n, int k, int cover[], int start, int currentSize,
 }
 
 void findMinVertexCover(int graph[MAX_VERTICES][MAX_VERTICES], int n) {
-    int* minCover = (int*)malloc(n * sizeof(int));
+    int* minCover = (int*)malloc(n * sizeof(int)); // The maximum vertex cover can't be larger than n/2
+    // cover is an array that stores the current combination of vertices we're considering as a vertex cover
     int* cover = (int*)malloc(n * sizeof(int));
-    int minSize = n + 1;
+    int minSize = n + 1; // n/2 +1 
 
     for (int k = 1; k <= n && minSize > k; k++) {
         generateCombinations(n, k, cover, 0, 0, graph, minCover, &minSize);
